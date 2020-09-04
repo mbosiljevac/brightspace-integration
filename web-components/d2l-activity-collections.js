d3fa360c08d5ad2e4961eb7327d1347abb4063f2
Maya Jones <Maya.Jones@D2L.com>
2020-09-04 12:56:13 -0400

    Use the activity editor

--- web-components/d2l-activity-collections.js
+++ web-components/d2l-activity-collections.js
@@ -1,2 +1,17 @@
 import 'd2l-organizations/components/d2l-organization-admin-list/d2l-organization-admin-list.js';
-import 'd2l-activities/components/d2l-activity-collection-editor/d2l-activity-collection-editor.js';
+import '@brightspace-ui/polaris/components/activity/editor/d2l-activity-editor.js';
+import { html, LitElement } from 'lit-element/lit-element.js';
+
+class CollectionEditor extends LitElement {
+	static get properties() {
+		return {
+			href: { type: String },
+			token: { type: String }
+		};
+	}
+
+	render() {
+		return html`<d2l-activity-editor href="${this.href}" .token="${this.token}"></d2l-activity-editor>`;
+	}
+}
+customElements.define('d2l-activity-collection-editor', CollectionEditor);
