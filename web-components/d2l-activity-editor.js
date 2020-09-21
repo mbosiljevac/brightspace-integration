baa2be2b392e52f5a0f87c71c9ad543ba493e5a8
Zina Ramirez <instinctualart@gmail.com>
2020-09-21 12:12:29 -0400

    Replace assignment editor with POC activity editor

--- web-components/d2l-activity-editor.js
+++ web-components/d2l-activity-editor.js
@@ -1,2 +1,16 @@
-import 'd2l-activities/components/d2l-activity-editor/d2l-activity-assignment-editor/d2l-activity-assignment-editor.js';
+import '@brightspace-ui/polaris/components/activity/editor/d2l-activity-editor.js';
+import { html, LitElement } from 'lit-element/lit-element.js';
 
+class CollectionEditor extends LitElement {
+	static get properties() {
+		return {
+			href: { type: String },
+			token: { type: String }
+		};
+	}
+
+	render() {
+		return html`<d2l-activity-editor no-header template="primary-secondary" href="${this.href}" .token="${this.token}"></d2l-activity-editor>`;
+	}
+}
+customElements.define('d2l-activity-assignment-editor', CollectionEditor);
