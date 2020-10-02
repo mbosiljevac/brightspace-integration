3a8099ac45a4dbf9ff8571389816c3489f8d8d65
Adam Kulas <33091324+adkulas@users.noreply.github.com>
2020-10-02 13:51:52 -0400

    Update Serge.test.js

--- test/Serge.test.js
+++ test/Serge.test.js
@@ -4,18 +4,18 @@ import { ForbiddenCharacters } from '../oslo/Util.js';
 
 describe('Serge parsing', () => {
 
-	const serge = new Serge('/root', 'test', '/root/test', {
-		name: 'test',
-		source_dir: 'src',
-		output_file_path: 'out'
-	});
+    const serge = new Serge('/root', 'test', '/root/test', {
+        name: 'test',
+        source_dir: 'src',
+        output_file_path: 'out'
+    });
 
-	describe('js', () => {
+    describe('js', () => {
 
-		const termNames = ['txtTerm\\u0020One', 'txtTermTwo'];
+        const termNames = ['txtTerm\\u0020One', 'txtTermTwo'];
 
-		it('parses valid js', () => {
-			const js = `
+        it('parses valid js', () => {
+            const js = `
 				import something from 'something-else';
 				
 				// comment to be noisy
@@ -28,37 +28,37 @@ describe('Serge parsing', () => {
 				};
 			`;
 
-			serge._parserPlugin = 'parse_js';
-			const collection = serge._parse(js);
+            serge._parserPlugin = 'parse_js';
+            const collection = serge._parse(js);
 
-			expect(collection.name).to.equal('test\\test');
+            expect(collection.name).to.equal('test\\test');
 
-			for (const object of collection.objects) {
+            for (const object of collection.objects) {
 
-				expect(object.name).to.be.oneOf(termNames);
+                expect(object.name).to.be.oneOf(termNames);
 
-				switch (object.name) {
+                switch (object.name) {
 
-					case 'txtTerm\\u0020One':
-						expect(object.defaultValue).to.equal('Value "\'\'\'" 1');
-						expect(object.description).to.equal('txtTermOne description');
-						break;
+                    case 'txtTerm\\u0020One':
+                        expect(object.defaultValue).to.equal('Value "\'\'\'" 1');
+                        expect(object.description).to.equal('txtTermOne description');
+                        break;
 
-					case 'txtTermTwo':
-						expect(object.defaultValue).to.equal('"\'"Value: 2":,,\'"');
-						expect(object.description).to.equal('txtTermTwo description');
-						break;
+                    case 'txtTermTwo':
+                        expect(object.defaultValue).to.equal('"\'"Value: 2":,,\'"');
+                        expect(object.description).to.equal('txtTermTwo description');
+                        break;
 
-					default:
-						expect.fail('Unexpected term name in collections');
-				}
-			}
-		});
+                    default:
+                        expect.fail('Unexpected term name in collections');
+                }
+            }
+        });
 
-		it('throws error if a forbidden character is found in a term name', () => {
+        it('throws error if a forbidden character is found in a term name', () => {
 
-			for (const char of ForbiddenCharacters) {
-				const js = `
+            for (const char of ForbiddenCharacters) {
+                const js = `
 				import something from 'something-else';
 				
 				// comment to be noisy
@@ -70,9 +70,9 @@ describe('Serge parsing', () => {
 				};
 			`;
 
-				serge._parserPlugin = 'parse_js';
-				expect(() => serge._parse(js)).to.throw('OSLO error: Forbidden characters used in LangObject name');
-			}
-		});
-	});
+                serge._parserPlugin = 'parse_js';
+                expect(() => serge._parse(js)).to.throw('OSLO error: Forbidden characters used in LangObject name');
+            }
+        });
+    });
 });
